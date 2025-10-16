b323a2d8bad97dde8f6d5d35a5218141a5c2ef62
Bassell-Iddisah <bbasssell16@gmail.com>
2025-10-16 09:33:39 +0000

    feat(accessibility): Add BaseController for manual user-service testing

---
+++ skilltracker-services/user-service/src/main/java/com/amalitech/user/service/controller/BaseController.java
@@ -0,0 +1,13 @@
+package com.amalitech.user.service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseController {
+    @GetMapping("/ping")
+    public String ping() {
+        return "UserService Active ...";
+    }
+}
