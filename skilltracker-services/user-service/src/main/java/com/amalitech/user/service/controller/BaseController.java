c37924de6e2139b7814e6c30a16cfb5456593d7b
Bassell-Iddisah <bbasssell16@gmail.com>
2025-10-16 10:36:35 +0000

    feat(accessibility): Add BaseController for manual isolated health check

--- skilltracker-services/user-service/src/main/java/com/amalitech/user/service/controller/BaseController.java
+++ skilltracker-services/user-service/src/main/java/com/amalitech/user/service/controller/BaseController.java
@@ -1,5 +1,7 @@
 package com.amalitech.user.service.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BaseController {
     @GetMapping("/ping")
-    public String ping() {
-        return "UserService Active ...";
+    public ResponseEntity<String> ping() {
+        return new ResponseEntity<>("UserService Active ...", HttpStatus.OK);
     }
 }
