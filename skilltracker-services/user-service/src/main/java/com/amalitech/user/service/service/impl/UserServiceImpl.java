497a58fe5b8ff4a22b77caf47bf1f791cd78842d
Bassell-Iddisah <bbasssell16@gmail.com>
2025-10-18 17:07:44 +0000

    feat(UserServiceImpl): Implement code verification functionality

--- skilltracker-services/user-service/src/main/java/com/amalitech/user/service/service/impl/UserServiceImpl.java
+++ skilltracker-services/user-service/src/main/java/com/amalitech/user/service/service/impl/UserServiceImpl.java
@@ -2,89 +2,65 @@ package com.amalitech.user.service.service.impl;
 
 import com.amalitech.user.service.dto.UserRequestDTO;
 import com.amalitech.user.service.dto.UserResponseDTO;
-import com.amalitech.user.service.enums.roleEnum;
-import com.amalitech.user.service.enums.stateEnum;
-import com.amalitech.user.service.enums.tierEnum;
+import com.amalitech.user.service.mapper.UserMapper;
 import com.amalitech.user.service.model.User;
 import com.amalitech.user.service.repository.UserRepository;
 import com.amalitech.user.service.service.UserService;
 import com.amalitech.user.service.util.EmailUtil;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-
 import java.security.SecureRandom;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    @Autowired
-    private UserRepository repo;
-
-    @Autowired
-    private EmailUtil emailUtil;
+    private final UserRepository repo;
+    private final EmailUtil emailUtil;
 
     private Integer tempCode;
 
+    public UserServiceImpl(UserRepository repo, EmailUtil emailUtil) {
+        this.repo = repo;
+        this.emailUtil = emailUtil;
+        tempCode = 0;
+    }
+
     @Override
     @Transactional
-    public User createUser(User userdto) {
-
-        // Convert DTO -> Entity
-        User user = new User();
-        user.setEmail(userdto.getEmail());
-        user.setPasswordHash(userdto.getPasswordHash()); // update this with encoder when security arrives
-        user.setLanguage("en");
-        user.setTimezone("UTC");
-
+    public UserResponseDTO createUser(UserRequestDTO userdto) {
+        User user = UserMapper.toEntity(userdto);
         User savedUser = repo.save(user);
 
-
-        // Convert Entity → DTO
-        UserResponseDTO userResponse =  new UserResponseDTO(
-                savedUser.getId(),
-//                savedUser.getUsername(),
+        notifyUser(
                 savedUser.getEmail(),
-//                savedUser.getRole(),
-//                savedUser.getState(),
-//                savedUser.getPremiumTier(),
-                savedUser.getLanguage(),
-                savedUser.getTimezone());
-//                savedUser.getLastLoginAt(),
-//                savedUser.getUpdatedAt());
+                "Account created successfully!",
+                "Enter this verification code to verify your identity:" + generateCode());
 
-        notifyUser(savedUser.getEmail(), generateCode()); // send verification code before returning registered user
-
-        return new User().builder()
-                .email(savedUser.getEmail())
-                .language(savedUser.getLanguage())
-                .timezone(savedUser.getTimezone())
-                .build();
+        return UserMapper.toDto(savedUser);
     }
 
-    public User verifyCode(String code, String email){
+    @Override
+    public Optional<UserResponseDTO> verifyCode(String code, String email) {
         if(code.equals(tempCode.toString())){
-           return repo.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found by stated email"));
+            return repo.findByEmail(email)
+                    .map(UserMapper::toDto);
         }
-        return new User();
+
+        return Optional.empty();
     }
 
-
-
-    public String notifyUser(String toEmail, int code) {
+    public void notifyUser(String toEmail, String subject, String message) {
         emailUtil.sendEmail(
                 toEmail,
-                "Account created successfully!",
-                "Your account has been created successfully.\n Here is your 6-digit verification code: \n " +
-                        "Past this link in your address bar: " + "https://dcb46541311e.ngrok-free.app/api/v1/auth/verify/"+code+"/"+toEmail);
-
-        return "Email sent successfully";
+                subject,
+                message
+        );
     }
 
     public Integer generateCode() {
         SecureRandom random = new SecureRandom();
         tempCode = 100000 + random.nextInt(900000);
-        return tempCode; // generates a 6-digit number between 100000–999999
+        return tempCode;
     }
 }
