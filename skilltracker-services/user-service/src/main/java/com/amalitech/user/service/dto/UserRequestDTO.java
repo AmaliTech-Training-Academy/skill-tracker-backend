1350a887d35535dd105b199e713814d8045f74d2
Bassell-Iddisah <bbasssell16@gmail.com>
2025-10-19 22:22:55 +0000

    refactor(UserRequestDTO): remove default set fields

--- skilltracker-services/user-service/src/main/java/com/amalitech/user/service/dto/UserRequestDTO.java
+++ skilltracker-services/user-service/src/main/java/com/amalitech/user/service/dto/UserRequestDTO.java
@@ -1,21 +1,10 @@
 package com.amalitech.user.service.dto;
 
-import com.amalitech.user.service.enums.roleEnum;
-import com.amalitech.user.service.enums.stateEnum;
-import com.amalitech.user.service.enums.tierEnum;
 import lombok.Builder;
-import java.time.LocalDateTime;
 
 @Builder
 public record UserRequestDTO(
         String email,
-        String password,
-        Enum<roleEnum> role,
-        Enum<stateEnum> state,
-        Enum<tierEnum> PremiumTier,
-        String language,
-        String timezone,
-        LocalDateTime last_login_at,
-        LocalDateTime updatedAt
-
+        String username,
+        String password
 ) {}
