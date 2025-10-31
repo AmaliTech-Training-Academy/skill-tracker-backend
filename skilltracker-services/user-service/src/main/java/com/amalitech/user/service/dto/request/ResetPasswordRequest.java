ab2b738a3a33347f5f8e59be1f8912d375ec468f
kaytee07 <kofi.taylor07@gmail.com>
2025-10-31 15:08:45 +0000

    fix(auth): permit resend otp without authentication

--- skilltracker-services/user-service/src/main/java/com/amalitech/user/service/dto/request/ResetPasswordRequest.java
+++ skilltracker-services/user-service/src/main/java/com/amalitech/user/service/dto/request/ResetPasswordRequest.java
@@ -5,9 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-public record ResetPasswordRequest(@NotBlank String token,  @NotBlank
-@Email
-String email,
+public record ResetPasswordRequest(@NotBlank String token,
 
                                    @NotBlank
                                    @Size(min = 8, message = "Password must be at least 8 characters long")
