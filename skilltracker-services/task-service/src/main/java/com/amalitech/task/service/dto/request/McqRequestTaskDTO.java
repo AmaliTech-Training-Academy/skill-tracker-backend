13366a828352fee5995f0bf043dca510c8d2d2d6
Bassell-Iddisah <bbasssell16@gmail.com>
2025-11-03 10:41:13 +0000

    feature(MCQRequest): Add MCQRequest dto

---
+++ skilltracker-services/task-service/src/main/java/com/amalitech/task/service/dto/request/McqRequestTaskDTO.java
@@ -0,0 +1,19 @@
+package com.amalitech.task.service.dto.request;
+
+import com.amalitech.task.service.dto.TaskDTO;
+import com.amalitech.user.service.model.User;
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class McqRequestTaskDTO {
+    private String instruction_type;
+    private User user_details;
+    private TaskDTO task_details;
+    private List<String> constraints;
+}
