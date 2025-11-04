73e036944e10727c72c8e2395788e04605b936f0
Bassell-Iddisah <bbasssell16@gmail.com>
2025-11-04 07:21:17 +0000

    feature(MCQGeneration): Add responseDTO for MCQ generation

--- skilltracker-services/task-service/src/main/java/com/amalitech/task/service/dto/response/McqResponseDTO.java
+++ skilltracker-services/task-service/src/main/java/com/amalitech/task/service/dto/response/McqResponseDTO.java
@@ -1,4 +1,16 @@
 package com.amalitech.task.service.dto.response;
 
-public class McqResponseTaskDTO {
+import com.amalitech.task.service.dto.MCQquestionDTO;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class McqResponseDTO {
+    public List<MCQquestionDTO> mcqQuestion;
 }
