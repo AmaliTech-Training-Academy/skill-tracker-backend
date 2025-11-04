1e81db4c6189ac19826eaf64fdd07fbd7144b21d
Bassell-Iddisah <bbasssell16@gmail.com>
2025-11-04 07:24:55 +0000

    feature(MCQGeneration): Add contract for MCQ generation

--- skilltracker-services/task-service/src/main/java/com/amalitech/task/service/service/TaskService.java
+++ skilltracker-services/task-service/src/main/java/com/amalitech/task/service/service/TaskService.java
@@ -7,6 +7,8 @@ import com.amalitech.task.service.dto.response.AdminTaskDetailResponse;
 import com.amalitech.task.service.dto.response.AdminTaskSummaryResponse;
 import com.amalitech.task.service.dto.request.McqRequestTaskDTO;
 import com.amalitech.task.service.dto.response.McqResponseTaskDTO;
+import com.amalitech.task.service.dto.request.McqRequestDTO;
+import com.amalitech.task.service.dto.response.McqResponseDTO;
 import com.amalitech.task.service.exception.ResourceNotFoundException;
 import com.amalitech.task.service.model.enums.TaskDifficulty;
 import com.amalitech.task.service.model.enums.TaskType;
@@ -22,7 +24,7 @@ import java.util.UUID;
  */
 public interface TaskService {
 
-    McqResponseTaskDTO generateMCQ(McqRequestTaskDTO taskDTO);
+    McqResponseDTO generateMCQ(McqRequestDTO taskDTO) throws Exception;
 
     /**
      * Get personalized tasks for a user based on their skill and difficulty.
