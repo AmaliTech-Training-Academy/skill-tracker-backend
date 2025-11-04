84dfb73285019449c008cc9cee41531804106f95
Bassell-Iddisah <bbasssell16@gmail.com>
2025-11-04 07:19:45 +0000

    feature(GenerateMCQ): Add generateMCQ controller

--- skilltracker-services/task-service/src/main/java/com/amalitech/task/service/controller/TaskController.java
+++ skilltracker-services/task-service/src/main/java/com/amalitech/task/service/controller/TaskController.java
@@ -5,6 +5,8 @@ import com.amalitech.task.service.dto.TaskDTO;
 import com.amalitech.task.service.dto.request.McqRequestTaskDTO;
 import com.amalitech.task.service.dto.response.McqResponseTaskDTO;
 import com.amalitech.task.service.model.enums.TaskType;
+import com.amalitech.task.service.dto.request.McqRequestDTO;
+import com.amalitech.task.service.dto.response.McqResponseDTO;
 import com.amalitech.task.service.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -56,11 +58,11 @@ public class TaskController {
     }
 
     @PostMapping("/generate/mcq")
-    @PreAuthorize("isAuthenticated()")
-    public ResponseEntity<ApiResponse<McqResponseTaskDTO>> generateMCQTask(
-            @RequestBody McqRequestTaskDTO taskDTO
-    ) {
-        McqResponseTaskDTO task = taskService.generateMCQ(taskDTO);
+//    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<ApiResponse<McqResponseDTO>> generateMCQ(
+            @RequestBody McqRequestDTO taskDTO
+    ) throws Exception {
+        McqResponseDTO task = taskService.generateMCQ(taskDTO);
         return ResponseEntity.ok(ApiResponse.success("MCQ Task Generated Successfully", task, ""));
     }
 
