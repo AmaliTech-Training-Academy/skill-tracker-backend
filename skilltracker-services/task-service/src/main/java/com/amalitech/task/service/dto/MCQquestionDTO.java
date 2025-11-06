c4f508dfe031afc4c6ef3434281113ebb4d9d46c
Bassell-Iddisah <bbasssell16@gmail.com>
2025-11-06 16:17:27 +0000

    refactor(model matching): Update MCQquestionDTO to match MCQ Task generated

--- skilltracker-services/task-service/src/main/java/com/amalitech/task/service/dto/MCQquestionDTO.java
+++ skilltracker-services/task-service/src/main/java/com/amalitech/task/service/dto/MCQquestionDTO.java
@@ -1,8 +1,11 @@
 package com.amalitech.task.service.dto;
 
+import com.amalitech.task.service.model.TaskDefinition;
+import com.amalitech.task.service.model.content.TaskContent;
 import lombok.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @Getter
 @Setter
@@ -10,10 +13,17 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class MCQquestionDTO {
+    private UUID userId;
     private String question_number;
+    private String question_title;
+    private String question_description;
+    private String question_type;
     private String question_text;
     private int question_duration;
+    private String question_difficulty;
+    private TaskDefinition taskDefinition;
     private List<String> options;
+    private Integer xpReward;
     private String hint;
     private String correct_answer;
     private String explanation;
