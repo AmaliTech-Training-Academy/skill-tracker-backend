970867797e5ada92cbfc053e5028d8979d28c90f
Bassell-Iddisah <bbasssell16@gmail.com>
2025-11-06 08:26:37 +0000

    refactor(McqRequestDTO): replace fields with Task model field names

--- skilltracker-services/task-service/src/main/java/com/amalitech/task/service/dto/request/McqRequestDTO.java
+++ skilltracker-services/task-service/src/main/java/com/amalitech/task/service/dto/request/McqRequestDTO.java
@@ -1,6 +1,10 @@
 package com.amalitech.task.service.dto.request;
 
+import com.amalitech.task.service.dto.MCQquestionDTO;
 import com.amalitech.task.service.dto.TaskDTO;
+import com.amalitech.task.service.model.content.TaskContent;
+import com.amalitech.task.service.model.enums.TaskDifficulty;
+import com.amalitech.task.service.model.enums.TaskType;
 import com.amalitech.user.service.model.User;
 import lombok.*;
 
@@ -12,9 +16,13 @@ import java.util.List;
 @Getter
 @Setter
 public class McqRequestDTO {
-    private String instruction_type;
-    private User user_details;
-    private TaskDTO task_details;
-    private List<String> constraints;
-    private int no_of_questions;
+    private String title;
+    private String description;
+    private TaskType type;
+    private TaskDifficulty difficulty;
+    private String content;
+    private Integer xpReward;
+    private String interest;
+    private String difficulty_level;
+    private Integer no_of_questions;
 }
