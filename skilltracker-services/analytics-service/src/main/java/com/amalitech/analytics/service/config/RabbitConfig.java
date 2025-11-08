db41c9968ad8c112cff7a4d803045ffffe6347c7
thenoblet <appiahpatricknoblet@gmail.com>
2025-11-08 19:27:55 +0000

    remove duplicate dependencies

--- skilltracker-services/analytics-service/src/main/java/com/amalitech/analytics/service/config/RabbitConfig.java
+++ skilltracker-services/analytics-service/src/main/java/com/amalitech/analytics/service/config/RabbitConfig.java
@@ -1,14 +1,15 @@
 package com.amalitech.analytics.service.config;
 
-import com.amalitech.common.event.events.RabbitMQConstants;
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 /**
  * Configuration class for setting up RabbitMQ messaging components.
