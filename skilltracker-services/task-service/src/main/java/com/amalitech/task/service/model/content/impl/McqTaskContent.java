d31962a7c69d2ad41f51cba46f8b8c62aba60609
Bassell-Iddisah <bbasssell16@gmail.com>
2025-11-10 02:31:09 +0000

    refactor(unused code): Comment out unused validations

--- skilltracker-services/task-service/src/main/java/com/amalitech/task/service/model/content/impl/McqTaskContent.java
+++ skilltracker-services/task-service/src/main/java/com/amalitech/task/service/model/content/impl/McqTaskContent.java
@@ -33,16 +33,3 @@ public class McqTaskContent implements TaskContent {
     private int xpReward;
     private String explanation;
 }
-
-//    @NotBlank(message = "Question cannot be blank")
-//    private String question;
-//
-//    @NotNull(message = "Options cannot be null")
-//    @Size(min = 2, message = "At least two options are required")
-//    private List<@NotBlank(message = "Option text cannot be blank") String> options;
-//
-//    @Min(value = 0, message = "Correct option index must be 0 or higher")
-//    private int correctOption; // index of the correct answer
-//
-//    @NotBlank(message = "Explanation cannot be blank")
-//    private String explanation;
