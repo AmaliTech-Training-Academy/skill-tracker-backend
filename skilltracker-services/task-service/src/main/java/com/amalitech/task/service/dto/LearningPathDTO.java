fefb589211bef2e44e4eb0607936f97d56097349
Bassell-Iddisah <bbasssell16@gmail.com>
2025-11-11 19:19:49 +0000

    feature(learning path): Add generated learning path structure

---
+++ skilltracker-services/task-service/src/main/java/com/amalitech/task/service/dto/LearningPathDTO.java
@@ -0,0 +1,22 @@
+package com.amalitech.task.service.dto;
+
+import lombok.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LearningPathDTO {
+    private UUID userId;
+    private String summary;
+    private String current_skill;
+    private String recommended_next_skill;
+    private List<String> recommended_activities;
+    private String reasoning;
+    private List<String> resources;
+    private String difficulty;
+}
