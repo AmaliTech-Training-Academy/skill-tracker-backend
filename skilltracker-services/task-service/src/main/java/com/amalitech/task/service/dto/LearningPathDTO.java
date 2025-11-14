4d1617eb9ce8323315767b5a7267dc9b31a87cda
Bassell-Iddisah <bbasssell16@gmail.com>
2025-11-14 07:01:11 +0000

    feature(resources): Add field for learning resources

--- skilltracker-services/task-service/src/main/java/com/amalitech/task/service/dto/LearningPathDTO.java
+++ skilltracker-services/task-service/src/main/java/com/amalitech/task/service/dto/LearningPathDTO.java
@@ -3,7 +3,6 @@ package com.amalitech.task.service.dto;
 import lombok.*;
 
 import java.util.List;
-import java.util.UUID;
 
 @Builder
 @Getter
@@ -11,11 +10,12 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LearningPathDTO {
-    private UUID userId;
+    private String userId;
     private String summary;
     private String current_skill;
     private String recommended_next_skill;
     private List<String> recommended_activities;
     private String reasoning;
     private String difficulty;
+    private List<String> resources;
 }
