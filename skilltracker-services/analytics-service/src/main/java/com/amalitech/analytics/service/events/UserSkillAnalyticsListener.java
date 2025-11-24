c4b031861de24961faed441ac017e30a6de68061
Patrick A. Noblet <appiahpatricknoblet@gmail.com>
2025-11-24 12:40:49 +0000

    Remove setting of selected level in UserSkillAnalyticsListener

--- skilltracker-services/analytics-service/src/main/java/com/amalitech/analytics/service/events/UserSkillAnalyticsListener.java
+++ skilltracker-services/analytics-service/src/main/java/com/amalitech/analytics/service/events/UserSkillAnalyticsListener.java
@@ -47,7 +47,6 @@ public class UserSkillAnalyticsListener {
                     s.setUserId(userId);
                     s.setSkillName(data.getSkillName());
                     s.setInitialClaimLevel(data.getDifficultyLevel());
-                    s.setSelectedLevel(data.getDifficultyLevel());
                     s.setSupportedTaskTypes(data.getSupportedTaskTypes());
                     s.setEvent(skillEvent);
                     return s;
